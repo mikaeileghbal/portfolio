459808bcaeb6eb8002e0b61100bdbdb06b3f5e99
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-02-22 14:25:24 +0330

    Add slide menu
    See: #1

--- code/index.js
+++ code/index.js
@@ -2,4 +2,9 @@ const menu = document.querySelector(".menu-container");
 
 menu.addEventListener("click", (e) => {
   menu.classList.toggle("change");
+  if (menu.classList.contains("change")) {
+    document.querySelector(".sidenav").style.height = "100%";
+  } else {
+    document.querySelector(".sidenav").style.height = "0";
+  }
 });
