1c69e18e4568bdb3d3262abb84c00bbdde8d5974
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-02-25 15:56:27 +0330

    Fix header, slide menu and hero
    See: #1

--- code/index.js
+++ code/index.js
@@ -1,10 +1,12 @@
-const menu = document.querySelector(".menu-container");
+const app = (function () {
+  const menu = document.querySelector(".menu-container");
 
-menu.addEventListener("click", (e) => {
-  menu.classList.toggle("change");
-  if (menu.classList.contains("change")) {
-    document.querySelector(".sidenav").style.height = "100%";
-  } else {
-    document.querySelector(".sidenav").style.height = "0";
-  }
-});
+  menu.addEventListener("click", (e) => {
+    menu.classList.toggle("change");
+    if (menu.classList.contains("change")) {
+      document.querySelector(".sidenav").style.height = "100%";
+    } else {
+      document.querySelector(".sidenav").style.height = "0";
+    }
+  });
+})();
