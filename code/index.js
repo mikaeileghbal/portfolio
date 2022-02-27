ff3ce5238262f468ad4c1a014de7878cf07b4ff9
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-02-28 00:29:17 +0330

    Fix slide menu
    See: #1

--- code/index.js
+++ code/index.js
@@ -1,12 +1,39 @@
-const app = (function () {
+const App = (function () {
   const menu = document.querySelector(".menu-container");
 
-  menu.addEventListener("click", (e) => {
+  function closeSideMenu() {
+    document.querySelector(".topnav").style.right = "0";
+  }
+
+  function openSideMenu() {
+    document.querySelector(".topnav").style.right = "-100%";
+  }
+
+  menu.addEventListener("click", slideMenu);
+
+  function slideMenu(event) {
+    event.stopPropagation();
     menu.classList.toggle("change");
     if (menu.classList.contains("change")) {
-      document.querySelector(".sidenav").style.height = "100%";
+      closeSideMenu();
     } else {
-      document.querySelector(".sidenav").style.height = "0";
+      openSideMenu();
     }
-  });
+  }
+
+  document.body.addEventListener("click", bodyClick, false);
+
+  function bodyClick(event) {
+    if (menu.classList.contains("change")) {
+      slideMenu(event);
+    }
+  }
+
+  window.addEventListener("resize", resize);
+
+  function resize(event) {
+    if (menu.classList.contains("change") && window.innerWidth >= 768) {
+      slideMenu(event);
+    }
+  }
 })();
