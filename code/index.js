05ffbb7fbe272e96fe23a266faed147928a0f326
Mikaeil <mikaeileghbal@yahoo.com>
2022-03-19 15:14:23 +0330

    Fix header style when scroll

--- code/index.js
+++ code/index.js
@@ -1,39 +1,48 @@
 const App = (function () {
-  const menu = document.querySelector(".menu-container");
+	const menu = document.querySelector(".menu-container");
 
-  function closeSideMenu() {
-    document.querySelector(".topnav").style.right = "0";
-  }
+	function closeSideMenu() {
+		document.querySelector(".topnav").style.right = "0";
+	}
 
-  function openSideMenu() {
-    document.querySelector(".topnav").style.right = "-100%";
-  }
+	function openSideMenu() {
+		document.querySelector(".topnav").style.right = "-100%";
+	}
 
-  menu.addEventListener("click", slideMenu);
+	menu.addEventListener("click", slideMenu);
+	menu.addEventListener("click", animateMenu);
+	function slideMenu(e) {
+		e.stopPropagation();
+		menu.classList.toggle("change");
+		if (menu.classList.contains("change")) {
+			closeSideMenu();
+		} else {
+			openSideMenu();
+		}
+	}
+	function animateMenu() {}
 
-  function slideMenu(event) {
-    event.stopPropagation();
-    menu.classList.toggle("change");
-    if (menu.classList.contains("change")) {
-      closeSideMenu();
-    } else {
-      openSideMenu();
-    }
-  }
+	document.body.addEventListener("click", bodyClick, false);
+	function bodyClick(e) {
+		if (menu.classList.contains("change")) {
+			slideMenu(e);
+		}
+	}
 
-  document.body.addEventListener("click", bodyClick, false);
+	window.addEventListener("resize", resize);
+	function resize(e) {
+		if (menu.classList.contains("change") && window.innerWidth >= 768) {
+			slideMenu(e);
+		}
+	}
 
-  function bodyClick(event) {
-    if (menu.classList.contains("change")) {
-      slideMenu(event);
-    }
-  }
-
-  window.addEventListener("resize", resize);
-
-  function resize(event) {
-    if (menu.classList.contains("change") && window.innerWidth >= 768) {
-      slideMenu(event);
-    }
-  }
+	window.addEventListener("scroll", windowScroll);
+	function windowScroll(e) {
+		const header = document.getElementById("header");
+		if (document.documentElement.scrollTop > 20) {
+			header.classList.add("slide");
+		} else {
+			header.classList.remove("slide");
+		}
+	}
 })();
